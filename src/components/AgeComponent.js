a2f96e203799c67646daba8403c966b9e8b4ce43
Nick Alberts <nwalberts5@gmail.com>
2022-07-01 14:51:58 -0400

    with component

---
+++ src/components/AgeComponent.js
@@ -0,0 +1,32 @@
+import React from "react"
+
+const AgeComponent = (props) => {
+
+    // const myAge = 34
+    // const ageOfMyFriend = 25
+
+    let whoIsOlder
+    if (props.myAge > props.ageOfMyFriend) {
+        whoIsOlder = "I am older"
+    } else {
+        whoIsOlder = "My friend is older"
+    }
+
+    const displayWhoIsOlderCallbackFunction = () => {
+        alert(whoIsOlder)
+    }
+
+    const questionToDisplay = "Click me to determine who is older: my friend, or me!"
+
+
+    return(
+        <div onClick={displayWhoIsOlderCallbackFunction}>
+            <p>
+                {questionToDisplay}
+            </p>
+        </div>
+    )
+}
+
+
+export default AgeComponent
