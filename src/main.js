5f8c7ba1201b4c4954738352dc50453b0da45702
Nick Alberts <nwalberts5@gmail.com>
2023-12-22 14:46:57 -0500

    new base

--- src/main.js
+++ src/main.js
@@ -1,74 +1,29 @@
 import React from "react"
 import ReactDOM from "react-dom"
 
-import GroceryMessage from "./components/GroceryMessage"
+console.log("Hello from the React files")
 
 
+// let timeBeforeGameOfThronesIsOn = 30
+// let groceryMessage
 
-// const response = await fetch("/api/v1/secret-message")
-// const messageFromTheBackend = await response.json()
-
-const messageFromTheBackend = "Drink your ovaltine!"
-
-// console.log("Hello from the React files")
-
-ReactDOM.render(
-    // GroceryMessage({ message: messageFromTheBackend }),    
-    <GroceryMessage 
-        message={messageFromTheBackend} 
-        otherText={"Foobar"} 
-    />,    
-    document.getElementById("app")
-)
-
-
-
-
-// create two arbitrary variables
-// because they aren't going to change in this program, use const
-// const myAge = 34
-// const ageOfMyFriend = 25
-
-// -------------
-// the value of whoIsOlder is dependent on the conditional
-// we create the variable with no value, and assign accordingly
-// let whoIsOlder;
-// if (myAge > ageOfMyFriend) {
-//     whoIsOlder = "I am older"
+// if (timeBeforeGameOfThronesIsOn > 20) {
+//     groceryMessage = "You can go grocery shopping if you are quick"
 // } else {
-//     whoIsOlder = "My friend is older"
+//     groceryMessage = "Don't even try. Just sit here and watch your show without snacks"
 // }
 
-// -------------
-// a callback function that will be passed to an event listener
-// the function has access to variables in the outer scope
-// - first it looks inside of the function for "who is older"
-// - then it looks in the outer scope for who is older
-// const displayWhoIsOlderCallbackFunction = () => {
-//     alert(whoIsOlder)
+// const greeting = () => {
+//     alert("YO! IM A REACT CALLBACK FUNCTION!")
 // }
 
-// -------------
-// JSX looks like HTML, but allows for JavaScript
-// this includes assigning the JSX to a variable for later
-// we can also display the values of variables with curly brackets
-// const questionToDisplay = "Click me to determine who is older: my friend, or me!"
+// const divElement = <div>
+//   <p>
+//       {groceryMessage}
+//   </p>
+// </div>;
 
-// const divElement =
-//     <div onClick={onClickCallback}>
-//         <p>
-//             {questionToDisplay}
-//         </p>
-//     </div>;
-
-// -------------
-// render is a method with two arguments
-// - what we want to render (some JSX element)
-// - where we want to render it (somewhere in the index.html)
 // ReactDOM.render(
 //     divElement,
 //     document.getElementById('app')
 // )
-
-// ------
-// now let's separate our concerns and make a component...
